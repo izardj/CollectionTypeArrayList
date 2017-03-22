8937f2c96e72251b906e81079ee3ff6af324c498
izardj <izard.jerome@free.fr>
2017-03-22 16:05:07 +0100

    final

---
+++ src/presentation/Lanceur.java
@@ -0,0 +1,27 @@
+package presentation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lanceur {
+
+	public static void main(String[] args) {
+
+		// declaration
+		List al = new ArrayList();
+
+		// ajout elements
+		al.add(12);
+		al.add("bonjour");
+		al.add(12.5f);
+		al.add(true);
+		al.add("d");
+		al.add("d");
+
+		// parcours
+		for (int i = 0; i < al.size(); i++) {
+			System.out.println("donnée à l'indice " + i + " : " + al.get(i));
+		}
+	}
+
+}
